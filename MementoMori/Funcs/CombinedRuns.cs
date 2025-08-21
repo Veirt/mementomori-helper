57ba6ac9fcb331ca989ce8907f076ccd0b0247fe
Veirt <veirt@duck.com>
2025-08-21 17:00:27 +0800

    feat: concurrently run main quest and tower of infinity

---
+++ MementoMori/Funcs/CombinedRuns.cs
@@ -0,0 +1,18 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MementoMori.Funcs;
+
+public partial class MementoMoriFuncs
+{
+    public async Task AutoBossAndInfiniteTowerRequest(long targetBossQuestId = 0, long targetTowerStopLayer = 0)
+    {
+        if (SelectedAutoTowerType != TowerType.Infinite)
+            SelectedAutoTowerType = TowerType.Infinite;
+
+        var bossTask = AutoBossRequest(targetBossQuestId);
+        var towerTask = AutoInfiniteTowerRequest(targetTowerStopLayer);
+
+        await Task.WhenAll(bossTask, towerTask);
+    }
+}
